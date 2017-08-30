bffbd2bcea3a8b6e01b77c1899da121b2f38ef18
Patrick Malolepszy <patmalolepszy@gmail.com>
2017-08-30 17:22:34 -0400

    Initial Commit

---
+++ app/src/main/java/team/tasktacker/MainActivity.java
@@ -0,0 +1,21 @@
+package team.tasktacker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
