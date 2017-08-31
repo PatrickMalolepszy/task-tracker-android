491118cb3e5d3aafe9943fd578e6a6969ae2d2ec
Patrick Malolepszy <patmalolepszy@gmail.com>
2017-08-30 22:53:01 -0400

    Added a basic form.

---
+++ app/src/main/java/models/BasicInfo.java
@@ -0,0 +1,12 @@
+package models;
+
+/**
+ * Created by Patrick on 2017-08-30.
+ */
+
+public class BasicInfo {
+
+    public String email;
+    public String phone;
+    public String date;
+}
--- app/src/main/java/team/tasktacker/MainActivity.java
+++ app/src/main/java/team/tasktacker/MainActivity.java
@@ -1,16 +1,49 @@
 package team.tasktacker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.welcome_text)
+    TextView welcomeMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "hi over here", Toast.LENGTH_SHORT).show();
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "Pausing main activity", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "Resuming main activity", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @OnClick(R.id.go_to_another)
+    public void goTo() {
+        welcomeMessage.getText();
+        Intent i = new Intent(getApplicationContext(), AnotherActivity.class);
+        startActivity(i);
     }
 
     @Override
